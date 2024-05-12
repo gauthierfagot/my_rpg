0e37674163a1e2f26809578360b85e20a70163b8
57ave <gustave.delecroix@epitech.eu>
2024-05-12 19:22:08 +0200

    feat: create structure inventory

---
+++ include/ecs/component/inventory_component.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** my_rpg
+** File description:
+** inventory_component.h
+*/
+
+#pragma once
+
+typedef struct inventory_s {
+    size_t size;
+    int *tab;
+} c_inventory_t;
