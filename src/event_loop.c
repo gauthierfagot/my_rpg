81ece48b98e54b26bc0d344f6876a0472fa912dc
Yanis <yanis.kernoua@epitech.eu>
2024-05-24 15:47:16 +0200

    feat(ecs/events): looping through action and executing their associated key

--- src/event_loop.c
+++ src/event_loop.c
@@ -8,6 +8,7 @@
 #include <SFML/Graphics.h>
 #include <SFML/System.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include "components.h"
@@ -19,11 +20,28 @@
 #include "filter_entity.h"
 #include "systems.h"
 
+static void execute_keys(entity_system_t *es, sfEvent *event)
+{
+    for (int i = 0; i < ACTION_COUNT; i++) {
+        if (es->keys[es->game_mode].key_map[i] == NULL) {
+            continue;
+        }
+        if (event->type == sfEvtKeyPressed &&
+            event->key.code == es->keys[es->game_mode].key_map[i]->key) {
+            es->keys[es->game_mode].key_map[i]->callback(es);
+            return;
+        }
+    }
+}
+
 static void handle_event(sfRenderWindow *wnd,
-    sfEvent *event, entity_system_t *)
+    sfEvent *event, entity_system_t *es)
 {
     if (event->type == sfEvtClosed || sfKeyboard_isKeyPressed(sfKeyEscape)) {
         sfRenderWindow_close(wnd);
+    }
+    if (event->type == sfEvtKeyPressed) {
+        execute_keys(es, event);
     }
 }
 
